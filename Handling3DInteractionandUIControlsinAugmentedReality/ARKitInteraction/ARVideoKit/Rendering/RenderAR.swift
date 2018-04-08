d01bfdfe80bbedc8da7010fc2dbab9a356410079
mac126 <tulaomao152@126.com>
2018-04-08 14:36:35 +0800

    更新ARVideoKit

--- Handling3DInteractionandUIControlsinAugmentedReality/ARKitInteraction/ARVideoKit/Rendering/RenderAR.swift
+++ Handling3DInteractionandUIControlsinAugmentedReality/ARKitInteraction/ARVideoKit/Rendering/RenderAR.swift
@@ -1,4 +1,4 @@
- //
+//
 //  RenderAR.swift
 //  ARVideoKit
 //
@@ -30,6 +30,7 @@ internal struct RenderAR {
         }
         return nil
     }
+    
     internal var bufferSize:CGSize? {
         guard let raw = rawBuffer else{return nil};
         var width = CVPixelBufferGetWidth(raw);
