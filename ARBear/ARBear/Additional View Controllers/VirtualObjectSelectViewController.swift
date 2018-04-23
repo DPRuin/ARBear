d53b1f7dddf3a827a3a4be01d38165f862e346e6
mac126 <tulaomao152@126.com>
2018-04-23 12:14:22 +0800

    底部弹出选择界面

--- ARBear/ARBear/Additional View Controllers/VirtualObjectSelectViewController.swift
+++ ARBear/ARBear/Additional View Controllers/VirtualObjectSelectViewController.swift
@@ -8,12 +8,21 @@
 
 import UIKit
 
-class VirtualObjectSelectViewController: UIViewController {
+class VirtualObjectSelectViewController: UIViewController, PresentBottomType {
 
+    var contentHeight: CGFloat {
+        return 300
+    }
+    
+    var canPanDown: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.automaticallyAdjustsScrollViewInsets = false
+        setupPageCollection()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +30,50 @@ class VirtualObjectSelectViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func setupPageCollection()  {
+        let titles = ["我的", "小熊"]
+        
+        var style = HFPageStyle()
+        style.isShowBottomLine = true
+        
+        let layout = HFPageCollectionLayout()
+        layout.scrollDirection = .horizontal
+        layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
+        layout.colMargin = 8
+        layout.rowMargin = 8
+        
+        let frame = CGRect(x: 0, y: 0, width: view.bounds.width, height: 300)
+        let pageCollection = HFPageCollectionView(frame: frame, titles: titles, isTop: true, style: style, layout: layout)
+        pageCollection.dataSource = self
+        pageCollection.delegate = self
+        
+        pageCollection.registerNib(UINib(nibName: "VirtualObjectCollectionViewCell", bundle: nil), reusableIdentify: "cellCollection")
+        view.addSubview(pageCollection)
     }
-    */
 
+}
+
+extension VirtualObjectSelectViewController : HFPageCollectionViewDataSource{
+    
+    func numberOfSectionsInPageCollectionView(_ pageCollectionView: HFPageCollectionView) -> Int {
+        return 2
+    }
+    
+    func pageCollectionView(_ pageCollectionView: HFPageCollectionView, numberOfItemsIn section: Int) -> Int {
+        return Int(arc4random_uniform(16) + 6)
+    }
+    
+    func pageCollectionView(_ pageCollectionView: HFPageCollectionView, _ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellCollection", for: indexPath)
+        // cell.backgroundColor = UIColor.randomColor()
+        return cell
+    }
+}
+
+extension VirtualObjectSelectViewController: HFPageCollectionViewDelegate {
+    func pageCollectionView(_ pageCollectionView: HFPageCollectionView, _ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        print("-----")
+        print("index-\(indexPath)")
+    }
 }
